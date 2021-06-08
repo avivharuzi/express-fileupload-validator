7fa65d875379cbaa30298d52f3049c73c5450049
Aviv Haruzi <avivharuzi@gmail.com>
2021-06-09 00:36:15 +0300

    Add main index.ts

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { ExpressFileuploadValidator } from './express-fileupload-validator';
+
+export * from './errors';
+export * from './types';
+
+export default ExpressFileuploadValidator;
+export { ExpressFileuploadValidator };
+
+module.exports = ExpressFileuploadValidator;
+module.exports.default = ExpressFileuploadValidator;
